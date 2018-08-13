0de0680239e84e46518a0d6e75f3efc5e1910a4a
Dobermensch <asharma@connect.ust.hk>
2018-08-13 23:20:10 +0800

    finished short code test

--- app.py
+++ app.py
@@ -1,10 +1,55 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+import time
+import random
+import threading
 
+app = Flask(__name__)
+socketio = SocketIO(app, logger = True, async_mode="threading")
+
+class ThreadingExample(object):
+    """ Threading example class
+    The run() method will be started and it will run in the background
+    until the application exits.
+    """
+
+    def __init__(self, interval=5):
+        """ Constructor
+        :type interval: int
+        :param interval: Check interval, in seconds
+        """
+        self.interval = interval
+
+        thread = threading.Thread(target=self.run, args=())
+        thread.daemon = True                            # Daemonize thread
+        thread.start()                                  # Start the execution
+
+    def run(self):
+        """ Method that runs forever """
+        while True:
+            
+            newLottery()
+            
+            time.sleep(self.interval)
+
+marksix = []
+
+def newLottery():
+    global marksix
+
+    marksix = []
+    for x in range(6):
+        marksix.append(random.randint(1,49))
+
+    print("new lottery is: " +  str(marksix))
+    socketio.emit('new_marksix', {"arr": marksix}, broadcast=True)
+    return marksix
 
 @app.route('/')
 def hello():
-    return "Hello World!"
+    return render_template("index.html")    
 
 if __name__ == '__main__':
-    app.run()
+    test = ThreadingExample()
+    socketio.run(app)    
+        
